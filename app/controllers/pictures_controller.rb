e0f051a6752a22094d97f0b8b2e2b08dbf5e29ec
akira-yp <yog.puripuri@gmail.com>
2021-02-03 00:16:57 +0900

    picture編集・削除機能、画像投稿機能を追加

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,20 +1,42 @@
 class PicturesController < ApplicationController
+  before_action :set_picture,only:[:show,:edit,:update,:destroy]
   def index
     @pictures = Picture.all
   end
   def new
-    @picture = Picture.new
+    @picture = current_user.pictures.new
   end
   def create
-    @picture = Picture.new(pictue_params)
+    @picture = current_user.pictures.new(picture_params)
     if @picture.save
       redirect_to pictures_path, notice:"投稿しました"
     else
       render :new
     end
   end
+  def show
+  end
+  def edit
+  end
+  def update
+    if @picture.update(picture_params)
+      redirect_to picture_path(@picture.id), notice:"投稿を更新しました"
+    else
+      render :edit
+    end
+  end
+  def destroy
+    if @picture.destroy
+      recirect_to pictures_path, notice:"投稿を削除しました"
+    else
+      render :show
+    end
+  end
   private
   def picture_params
     params.require(:picture).permit(:content,:image,:image_cache)
+  end
+  def set_picture
+    @picture = Picture.find(params[:id])
   end
 end
