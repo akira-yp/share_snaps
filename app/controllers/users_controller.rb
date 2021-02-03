3eb001841968364db291d4fe0dc14cd353614b74
akira-yp <yog.puripuri@gmail.com>
2021-02-04 00:07:53 +0900

    プロフィール編集機能、プロフィール画像アップロード機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+  before_action :set_user,only:[:show,:edit,:update,:destroy]
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -10,12 +12,26 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
   def show
-    @user = User.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice:"ユーザー情報を変更しました"
+    else
+      render :edit
+    end
+  end
    private
    def user_params
-     params.require(:user).permit(:name, :email, :password,:password_confirmation)
+     params.require(:user).permit(:name, :email, :password,:password_confirmation,:avatar,:avatar_cache )
+   end
+
+   def set_user
+     @user = User.find(params[:id])
    end
 end
