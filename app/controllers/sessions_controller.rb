441df63d3652676fc59b696f2553b074c07fcf7a
akira-yp <yog.puripuri@gmail.com>
2021-02-11 00:11:59 +0900

    サインインとログインのデザインを作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by(params[:session][:email].downcase!)
     if user && user.authenticate(params[:session][:password])
       session[:user_id]= user.id
-      redirect_to user_path(user.id)
+      redirect_to users_path,notice:"ログインしました"
     else
       flash.now[:danger] = "ログインに失敗しました"
       render :new
