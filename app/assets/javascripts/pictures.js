500bb0affd89d6c188c5732636f09fd81d6a3ea8
akira-yp <yog.puripuri@gmail.com>
2021-02-17 22:54:55 +0900

    picture一覧にfavoritesの数を表示

--- app/assets/javascripts/pictures.js
+++ app/assets/javascripts/pictures.js
@@ -2,13 +2,13 @@ $(function(){
 
   $('.custom-file-input').on('change',function(){
   $(this).next('.custom-file-label').html($(this)[0].files[0].name);
-  })
+  });
 
   $('.square-image').mouseover(function(){
-    $(this).next().removeClass('.disappear');
+    $(this).next('.card-img-overlay').removeClass('disappear');
   })
   $('.square-image').mouseout(function(){
-    $(this).next().addClass('.disappear');
-  })
+    $(this).next('.card-img-overlay').addClass('disappear');
+  });
 
 });
